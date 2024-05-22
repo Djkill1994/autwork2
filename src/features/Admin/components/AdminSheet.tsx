8b02d6d1e6fdb4a0bd4c485ac8d3a9dc3dcf1309
Vlad <84898137+Djkill1994@users.noreply.github.com>
2024-05-22 21:26:12 +0300

    added admin sheet

--- src/features/Admin/components/AdminSheet.tsx
+++ src/features/Admin/components/AdminSheet.tsx
@@ -1,23 +1,71 @@
-import { Box, CircularProgress, Stack } from "@mui/material";
+import { Box, Button, CircularProgress, Stack } from "@mui/material";
 import { useGetUsersApi } from "~/features/Admin/api";
+import {
+  MaterialReactTable,
+  type MRT_ColumnDef,
+  useMaterialReactTable,
+} from "material-react-table";
+import { useMemo, useState } from "react";
+import { Database } from "~/generated/types/database";
 
 export const AdminSheet = () => {
-  const { data, isFetching } = useGetUsersApi();
+  const { data: usersData, isSuccess, isFetching } = useGetUsersApi();
+  const [editedCells, setEditedCells] = useState<Partial<Database>[]>([]);
+
+  const columns = useMemo<MRT_ColumnDef<Database>[]>(
+    () => [
+      {
+        accessorKey: "userName",
+        header: "User name",
+        size: 110,
+        enableEditing: false,
+      },
+      {
+        accessorKey: "email",
+        header: "Email",
+        size: 130,
+        // muiEditTextFieldProps: ({ row }) => ({
+        //   type: "text",
+        //   required: true,
+        //   onChange: (event) =>
+        //     handleEditCellChange(row.original, "project", event.target.value),
+        // }),
+      },
+    ],
+    [editedCells],
+  );
+  console.log(usersData?.users?.map((data) => data));
+  const table = useMaterialReactTable({
+    columns,
+    data: isSuccess && usersData?.users?.map((data) => data.user_metadata),
+    initialState: {
+      columnPinning: {
+        left: ["date"],
+      },
+      rowPinning: {
+        top: ["Total"],
+      },
+    },
+    muiTableBodyCellProps: {
+      sx: {
+        display: "flex",
+        alignItems: "center",
+      },
+    },
+    editDisplayMode: "cell",
+    enableEditing: true,
+    enableSorting: false,
+    enableColumnFilters: false,
+    enableColumnActions: false,
+    enableBottomToolbar: false,
+    enableColumnResizing: true,
+    enablePagination: false,
+    enableRowVirtualization: true,
+  });
 
   return (
     <Box>
-      {isFetching ? (
-        <CircularProgress />
-      ) : (
-        <Stack flexDirection="row" gap="8px">
-          {data?.users?.map(({ id, user_metadata }) => (
-            <Stack key={id}>
-              <Box>{user_metadata.email}</Box>
-              <Box>{user_metadata.userName}</Box>
-            </Stack>
-          ))}
-        </Stack>
-      )}
+      {isFetching ? <CircularProgress /> : <MaterialReactTable table={table} />}
     </Box>
   );
 };
